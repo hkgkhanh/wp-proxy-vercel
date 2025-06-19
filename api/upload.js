2724932acfdecaa2099e17f01a8c2200fcae060c
Nông Quốc Khánh <56966634+hkgkhanh@users.noreply.github.com>
2025-06-19 04:35:43 +0000

    fix CORS

--- api/upload.js
+++ api/upload.js
@@ -7,7 +7,7 @@ export const config = {
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Authorization');
+    res.setHeader('Access-Control-Allow-Headers', '*');
 
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
