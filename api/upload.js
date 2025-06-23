20856e747bcdf2e1f1b2f4810f616e6361f4adf7
Nông Quốc Khánh <56966634+hkgkhanh@users.noreply.github.com>
2025-06-23 04:36:03 +0000

    fix CORS

--- api/upload.js
+++ api/upload.js
@@ -5,10 +5,11 @@ export const config = {
 };
 
 export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type, site');
+
     if (req.method === 'OPTIONS') {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-        res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type, site');
         return res.status(200).end();
     }
 
