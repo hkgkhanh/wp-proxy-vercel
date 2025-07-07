faae4efecb310223b798440caa9eb77422d9211b
Nông Quốc Khánh <56966634+hkgkhanh@users.noreply.github.com>
2025-07-07 03:38:15 +0000

    add options request

--- api/x/post.js
+++ api/x/post.js
@@ -6,6 +6,10 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
     if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
 
     try {
