7da55689426db4adb206eb0fb4eba7240973c312
Nông Quốc Khánh <56966634+hkgkhanh@users.noreply.github.com>
2025-07-07 03:35:15 +0000

    fix CORS

--- api/x/upload.js
+++ api/x/upload.js
@@ -3,7 +3,7 @@ const { TwitterApi } = require('twitter-api-v2');
 
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
     if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
