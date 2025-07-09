fd19fd1e9c9f5597e20ab641120265c2040a9dd6
Nông Quốc Khánh <56966634+hkgkhanh@users.noreply.github.com>
2025-07-09 03:24:01 +0000

    check returned urn

--- api/li/me.js
+++ api/li/me.js
@@ -23,6 +23,7 @@ export default async function handler(req, res) {
         });
 
         const data = await response.json();
+        console.log(data);
 
         if (!response.ok) {
             return res.status(response.status).json({ error: data });
