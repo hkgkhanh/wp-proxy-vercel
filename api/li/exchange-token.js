68991d1044085a383141db018c6cf1f61e183206
Nông Quốc Khánh <56966634+hkgkhanh@users.noreply.github.com>
2025-07-09 01:42:59 +0000

    check li token return

--- api/li/exchange-token.js
+++ api/li/exchange-token.js
@@ -31,6 +31,7 @@ export default async function handler(req, res) {
         });
 
         const data = await response.json();
+        console.log(data);
 
         if (data.access_token) {
             res.status(200).json({ access_token: data.access_token });
