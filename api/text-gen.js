39cbe82c882268b0463cd0e31f24fd3a21aaa741
Nông Quốc Khánh <56966634+hkgkhanh@users.noreply.github.com>
2025-07-16 02:24:28 +0000

    change to @cf/qwen/qwen1.5-0.5b-chat model

--- api/text-gen.js
+++ api/text-gen.js
@@ -27,7 +27,7 @@ exports.default = async function handler(req, res) {
     // const HF_TOKEN = process.env.HF_TOKEN;
     const CF_ACCID = process.env.CLOUDFLARE_ACCID;
     const CF_TOKEN = process.env.CLOUDFLARE_TOKEN;
-    const model = "@hf/meta-llama/meta-llama-3-8b-instruct";
+    const model = "@cf/qwen/qwen1.5-0.5b-chat";
 
     const MODEL_ENDPOINT = `https://api.cloudflare.com/client/v4/accounts/${CF_ACCID}/ai/run/${model}`; // hoặc model khác
 
